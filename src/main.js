9122d7b379420d74ca42026e56ad764356126d32
Kazuki Nakamura <kajirikajiri@gmail.com>
2019-02-18 22:10:04 +0900

    commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+let app = '';
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyAmPwINtJ9c2qGiHP7shqrFjJVew7BpTVI",
+  authDomain: "vue-firebase-4054b.firebaseapp.com",
+  databaseURL: "https://vue-firebase-4054b.firebaseio.com",
+  projectId: "vue-firebase-4054b",
+  storageBucket: "vue-firebase-4054b.appspot.com",
+  messagingSenderId: "701721228624"
+})
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
---
+++ src/views/Login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" v-model="email" placeholder="Email">
+    <br>
+    <input type="password" v-model="password" placeholder="Password">
+    <br>
+    <button @click="login">Connection</button>
+    <p>You don't have an accout ? You can create one
+      <router-link to="/signup">create one</router-link>
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import firebase from 'firebase'
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'login',
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    login: function () {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+        (user) => {
+          alert('Well done ! You are now connected')
+          this.$router.replace('home')
+        },
+        (err) => {
+          alert('Oops ' + err.message)
+        }
+      )
+    }
+  }
+})
+</script>
+
+<style scoped>
+.login {
+  margin-top: 40px;
+}
+input {
+  margin: 10px 0;
+  width: 20%;
+  padding: 15px;
+}
+button {
+  margin-top: 20px;
+  width: 10%;
+  cursor: pointer;
+}
+p {
+  margin-top: 40px;
+  font-size: 13px;
+}
+p a {
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
